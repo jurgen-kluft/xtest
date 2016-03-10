60783af52efd9ce41ad2f8e2f0e428b03562aa66
Jurgen <jurgen@virtuos.com.cn>
2016-03-10 14:24:01 +0800

    *: adding dependency on xbase

--- package/package.go
+++ package/package.go
@@ -1,14 +1,18 @@
 package xtest
 
 import (
+	"github.com/jurgen-kluft/xbase/package"
 	"github.com/jurgen-kluft/xcode/denv"
 )
 
 // GetPackage returns the package object of 'xtest'
 func GetPackage() *denv.Package {
+	// dependencies
+	xbasepkg := xbase.GetPackage()
 
 	// 'xtest' application
 	mainapp := denv.SetupDefaultCppAppProject("xtest", "github.com\\jurgen-kluft\\xtest")
+	mainapp.Dependencies = append(mainapp.Dependencies, xbasepkg.GetMainLib())
 
 	mainpkg := denv.NewPackage("xtest")
 	mainpkg.AddMainApp(mainapp)
